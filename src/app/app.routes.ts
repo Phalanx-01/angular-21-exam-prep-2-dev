de860d05595a235c4a660c2dd57768bd72bfeaab
Koutsouridis Loukas (IT-DA-EXT - Extern) <loukas.koutsouridis@sbb.ch>
2026-03-17 15:15:47 +0100

    Basic routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    { path: 'dashboard', component: Dashboard },
+    { path: '**', component: NotFound }
+];
